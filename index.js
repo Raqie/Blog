b23d84cbd1e752d7c580fbe996cf682e0198b8cb
Raqie <37311846+Raqie@users.noreply.github.com>
2022-03-07 16:59:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'))
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render('home')
+})
+app.get("/about", (req,res) =>{
+    res.render('about')
+})
+app.listen(3000, () =>{
+    console.log("Listening on PORT 3000")
+})
